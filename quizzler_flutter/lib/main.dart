d04219d65eea3667cea98b3e223d97a917c4b6f7
Archanciel <jp.schnyder@gmail.com>
2022-05-01 15:54:43 +0200

    learning

---
+++ quizzler_flutter/lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  final List<Icon> _iconLst = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        const Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where the question text will go.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.circular(4)),
+              ),
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  _iconLst.add(const Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ));
+                }); //The user picked true.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+                shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.circular(4)),
+              ),
+              child: const Text(
+                'False',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  _iconLst.add(const Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  ));
+                }); //The user picked true.
+              },
+            ),
+          ),
+        ),
+        Row(children: _iconLst),
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
--- xylophone_flutter_master/lib/main.dart
+++ xylophone_flutter_master/lib/main.dart
@@ -16,13 +16,37 @@ class XylophoneApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              createTextButton(wavFileNb: 1, color: Colors.red),
-              createTextButton(wavFileNb: 2, color: Colors.orange),
-              createTextButton(wavFileNb: 3, color:Colors.yellow),
-              createTextButton(wavFileNb: 4, color:Colors.green),
-              createTextButton(wavFileNb: 5, color:Colors.teal),
-              createTextButton(wavFileNb: 6, color:Colors.blue),
-              createTextButton(wavFileNb: 7, color:Colors.purple),
+              Expanded(
+                child: TextButton(
+                  child: Text(''),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    shape:
+                        BeveledRectangleBorder(borderRadius: BorderRadius.zero),
+                  ),
+                  onPressed: () {
+                    audioPlayer.play('note1.wav');
+                  },
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  child: Text(''),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.orange,
+                    shape:
+                        BeveledRectangleBorder(borderRadius: BorderRadius.zero),
+                  ),
+                  onPressed: () {
+                    audioPlayer.play('note2.wav');
+                  },
+                ),
+              ),
+              createTextButton(wavFileNb: 3, color: Colors.yellow),
+              createTextButton(wavFileNb: 4, color: Colors.green),
+              createTextButton(wavFileNb: 5, color: Colors.teal),
+              createTextButton(wavFileNb: 6, color: Colors.blue),
+              createTextButton(wavFileNb: 7, color: Colors.purple),
             ],
           ),
         ),
