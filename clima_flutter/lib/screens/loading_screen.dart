768d6c8096904c610a69432d30841ae5e8d374ee
Archanciel <jp.schnyder@gmail.com>
2022-08-25 09:30:45 +0200

    Revert "entered duration can now be 1 or n digits withoutadding :00 toiit"
    This reverts commit 525c2360342f5ec39ff94f4adef003647770bef7.

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,21 +6,13 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    
-    print(position);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            getLocation();
+            //Get the current location
           },
           child: Text('Get Location'),
         ),
